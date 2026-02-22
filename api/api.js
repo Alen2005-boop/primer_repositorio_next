03a0f10d9b7df0317ce7f2ef80d0d9ee25269813
Alen2005-boop <alenravel1515@gmail.com>
2026-02-22 12:19:39 -0300

    34334

--- api/api.js
+++ api/api.js
@@ -1,4 +1,4 @@
-const URL ="https://api-react-taller-production.up.railway.app/" 
+const URL ="https://api-react-taller-production.up.railway.app" 
 // const TOKEN_KEY = "token"
 
 // export const saveToken = (token) => {
@@ -15,7 +15,7 @@ const URL ="https://api-react-taller-production.up.railway.app/"
 
 const register = async (username, name, password) => {
 
-    const response = await fetch('${URL}/api/auth/register', {
+    const response = await fetch('${URL}/api/api/register', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({ username, name, password })
@@ -26,7 +26,7 @@ const register = async (username, name, password) => {
 }
 
 const login = async (username, password) => {
-    const response = await fetch(`${URL}/api/auth/login`, {
+    const response = await fetch(`${URL}/api/api/login`, {
         method : "POST",
         headers:{"Content-Type" : "application/json"},
         body: JSON.stringify({username, password})
