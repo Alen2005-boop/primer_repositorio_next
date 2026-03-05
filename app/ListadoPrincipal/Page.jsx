1bbd9cc23303c5151f74b56e7eaed155e6ff49e1
Alen2005-boop <alenravel1515@gmail.com>
2026-03-05 19:12:18 -0300

    hghgj

---
+++ app/ListadoPrincipal/Page.jsx
@@ -0,0 +1,23 @@
+'use client'
+
+import ListadoPrincipalComponent from "../../components/ListadoPrincipal";
+
+import Header from "../../components/Header";
+
+
+const ListadoPrincipal = () => {
+
+
+    return(
+        <>
+        <Header/>
+        <ListadoPrincipalComponent/>
+
+        </>
+    );
+
+}
+
+
+export default ListadoPrincipal;
+
