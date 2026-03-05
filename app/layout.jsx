3f87a4cfb60b0dfdee48f81d5fad8b87e49af252
criscarro <cristiancarro2004@gmail.com>
2026-03-05 18:56:58 -0300

    a

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "./ListadoPrincipal/[id]/page"
+//import "./ListadoPrincipal/[id]/page"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
