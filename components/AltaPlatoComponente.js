a904cae5545dc9a50d8d2d7fcafefa6372e6824f
Alen2005-boop <alenravel1515@gmail.com>
2026-03-08 13:33:21 -0300

    rere

--- components/AltaPlatoComponente.js
+++ components/AltaPlatoComponente.js
@@ -159,31 +159,6 @@ const AltaPlatoComponent = () => {
                             />
                         </div>
                     </div>
-
-                    
-                    <div className="sm:col-span-2">
-                        <label htmlFor="photo" className="block text-sm/6 font-semibold text-gray-900">
-                            Add Photo URL
-                        </label>
-                        <div className="mt-2.5 flex gap-2">
-                            <input
-                                id="photo"
-                                name="photo"
-                                type="text"
-                                value={photo}
-                                onChange={(e) => setPhoto(e.target.value)}
-                                className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-                                placeholder="https://ejemplo.com/imagen.jpg"
-                            />
-                            <button
-                                onClick={handleAddPhoto}
-                                className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Add
-                            </button>
-                        </div>
-                    </div>
-
                     
                     {photos.length > 0 && (
                         <div className="sm:col-span-2">
