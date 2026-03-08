198f754d829ea3dcada66248c1d495217df58acc
Alen2005-boop <alenravel1515@gmail.com>
2026-03-07 21:07:00 -0300

    edede

--- components/AltaPlatoComponente.js
+++ components/AltaPlatoComponente.js
@@ -158,7 +158,7 @@ const AltaPlatoComponent = () => {
             onClick={handleClick}
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Alta Local
+            Alta Plato
           </button>
         </div>
       </form>
